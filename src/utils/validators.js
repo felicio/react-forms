902abf98247340a5e96982c6bdff81600c3ffa1e
Felicio Mununga <>
2017-10-02 16:05:56 +0200

    Add email validating funciton

---
+++ src/utils/validators.js
@@ -0,0 +1,6 @@
+/** Test validity of an email based on pattern set by regular expression. */
+export function emailIsValid(email) {
+  const regex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i
+
+  return regex.test(email)
+}
