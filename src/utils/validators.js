bdb2fce838fb10852d5b677016f5f2e14cda1fa1
Felicio Mununga <>
2017-10-03 12:24:37 +0200

    Add validating objects

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,3 +1,5 @@
+import { isNil } from 'ramda'
+
 /** Validate email based on pattern set with regular expression. */
 export function emailIsValid(email) {
   const regex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i
@@ -10,4 +12,24 @@ export function valueIsEmpty(value) {
   const regex = /\s*/
 
   return !value || !value.split(regex).join('')
+}
+
+export const required = {
+  fn: value => !isNil(value),
+  msg: 'This field is mandatory',
+}
+
+export const email = {
+  fn: value => /\S+@\S+\.\S+/.test(value),
+  msg: 'Must be correct email address',
+}
+
+export const minLength = length => ({
+  fn: value => value && value.length >= length,
+  msg: `Must be at least ${length} characters long`,
+})
+
+export const number = {
+  fn: value => /^-?\d+\.?\d*$/.test(value),
+  msg: 'Must be a valid number',
 }
