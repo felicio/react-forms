fcf3eb9c2540f01ae686e1e88d930a5731ebf31b
Felicio Mununga <felicio@users.noreply.github.com>
2017-10-04 12:14:11 +0200

    Add event form with data prop to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
 import LoginFormContainer from './containers/login-form-container'
+import EventForm from './components/event-form'
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <div style={{ padding: '0 10px' }}>
         <LoginFormContainer />
+        <EventForm data={{ title: 'Celebration of life', capacity: 7 }} />
       </div>
     )
   }
 }
+
+export default App
