9ae16c39d79e1a2f822c69205b8458e7203c6a77
Felicio Mununga <felicio@users.noreply.github.com>
2017-10-05 15:19:11 +0200

    Export Input and Label styled components

--- src/components/form-input.js
+++ src/components/form-input.js
@@ -7,12 +7,13 @@ const FormInput = ({
   name,
   type,
   required,
+  failure,
   defaultValue,
   focused,
   setFocused,
   handleChange,
 }) => (
-  <div style={{ marginBottom: '10px' }}>
+  <Wrapper>
     <Label htmlFor={name} focused={focused}>
       {name}:
     </Label>
@@ -20,35 +21,52 @@ const FormInput = ({
       id={name}
       name={name}
       type={type}
+      failure={failure}
       required={required}
       defaultValue={defaultValue}
       focused={focused}
       onFocus={() => setFocused(true)}
       onBlur={() => setFocused(false)}
       onChange={handleChange}
-      autoComplete="off"
     />
-  </div>
+  </Wrapper>
 )
+
+FormInput.defaultProps = {
+  required: false,
+  failure: false,
+}
 
 FormInput.propTypes = {
   defaultValue: PropTypes.any,
+  failure: PropTypes.bool,
+  handleChange: PropTypes.func.isRequired,
   name: PropTypes.string.isRequired,
-  required: PropTypes.bool.isRequired,
+  required: PropTypes.bool,
   type: PropTypes.string.isRequired,
 }
 
-export default compose(withState('focused', 'setFocused', false))(FormInput)
-
-const Label = styled.label`
-  margin-right: 10px;
+export const Label = styled.label`
   ${props => props.focused && css`font-weight: bolder;`};
 `
 
-const Input = styled.input`
+export const Input = styled.input.attrs({
+  autoComplete: 'off', // static prop
+})`
   width: 200px;
-  border: none;
+  height: 25px;
   outline: 0;
+  border: none;
   border-bottom: 1px solid lightgrey;
+  padding: 0 10px;
+  ${props => props.failure && css`border-bottom-color: red;`};
   ${props => props.focused && css`border-bottom-color: black;`};
 `
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: baseline;
+  margin-bottom: 10px;
+`
+
+export default compose(withState('focused', 'setFocused', false))(FormInput)
