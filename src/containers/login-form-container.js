bcd4c3ac007d59a35419fe71883449dabfdb2cc2
Felicio Mununga <felicio@users.noreply.github.com>
2017-10-05 18:27:26 +0200

    Refactor form methods

--- src/containers/login-form-container.js
+++ src/containers/login-form-container.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { emailIsValid, isEmpty } from '../utils/validators'
+
 import LoginForm, { inputs as formInputs } from '../components/login-form'
 
 // TODO: Handle change, validation and submitting in form's HoC.
@@ -12,7 +14,36 @@ class LoginFormContainer extends React.Component {
       errors: {},
       failure: false, // Received prop from a store.
     }
-    this.formIsValid = formIsValid.bind(this)
+  }
+
+  hasErrors = () => {
+    const { errors } = this.state
+
+    return Object.values(errors).filter(error => error).length
+  }
+
+  isFilled = () => {
+    const { inputs } = this.state
+
+    return Object.values(inputs).filter(value => value).length === 2
+  }
+
+  isValid = () => {
+    const { email, password } = this.state.inputs
+    let errors = {}
+
+    if (!emailIsValid(email)) {
+      errors['email'] = 'not valid'
+    }
+
+    if (false) {
+      errors['password'] = 'not valid'
+    }
+
+    // FIXME: Address previous state of errors.
+    return Object.keys(errors).length > 0
+      ? (this.setState({ errors }), false)
+      : true
   }
 
   handleChange = event => {
@@ -23,16 +54,32 @@ class LoginFormContainer extends React.Component {
     }))
   }
 
-  handleError = (name, value) => {
+  handleBlur = (name, value) => {
+    let error = { [name]: '' }
+
+    switch (name) {
+      case 'email':
+        if (isEmpty(value) || !emailIsValid(value)) {
+          error[name] = 'not valid'
+        }
+        break
+      case 'password':
+        if (isEmpty(value)) {
+          error[name] = 'not valid'
+        }
+      default:
+        break
+    }
+
     this.setState(prevState => ({
-      errors: { ...prevState.errors, [name]: value },
+      errors: { ...prevState.errors, ...error },
     }))
   }
 
   handleSubmit = event => {
     event.preventDefault()
 
-    if (this.formIsValid(formInputs)) {
+    if (this.isValid()) {
       const { email, password } = this.state.inputs
 
       const payload = {
@@ -45,38 +92,25 @@ class LoginFormContainer extends React.Component {
   }
 
   render() {
-    const { failure, errors } = this.state
+    const { failure, errors, inputs } = this.state
 
     return (
       <div>
         <h1>Login</h1>
         {failure && (
-          <small style={{ color: 'red' }}>
-            Incorrect email or password.
-          </small>
+          <small style={{ color: 'red' }}>Incorrect email or password.</small>
         )}
         <LoginForm
+          errors={errors}
           failure={failure}
+          filled={this.isFilled() && !this.hasErrors()}
+          handleBlur={this.handleBlur}
           handleChange={this.handleChange}
           handleSubmit={this.handleSubmit}
         />
       </div>
     )
   }
-}
-
-/** Validates all form inputs and optionally sets state of `errors` object. */
-function formIsValid(inputs) {
-  const errors = {}
-
-  inputs.forEach(
-    input =>
-      (errors[input.name] = input.validate(this.state.inputs[input.name])),
-  )
-
-  const errorValues = Object.values(errors).filter(error => error !== undefined)
-
-  return errorValues.length > 0 ? (this.setState({ errors }), false) : true
 }
 
 export default LoginFormContainer
